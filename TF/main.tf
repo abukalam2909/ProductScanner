9bb90ed282bf0087f4b1713686372975056b0b7f
Abu Kalam Babuji <abukalambabuji99@gmail.com>
2025-04-18 11:30:29 -0300

    Changed architecture from Fargate to EC2 for compute

--- TF/main.tf
+++ TF/main.tf
@@ -46,12 +46,26 @@ resource "aws_internet_gateway" "igw" {
   tags = { Name = "project-igw" }
 }
 
+resource "aws_eip" "nat" {
+  domain = "vpc"
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public1.id
+  tags = { Name = "project-nat-gateway" }
+  depends_on = [aws_internet_gateway.igw]
+}
+
 resource "aws_subnet" "public1" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
   availability_zone       = var.az1
   map_public_ip_on_launch = true
-  tags = { Name = "public-subnet-1" }
+  tags = {
+    Name                     = "public-subnet-1"
+    "kubernetes.io/role/elb" = "1"
+  }
 }
 
 resource "aws_subnet" "public2" {
@@ -59,7 +73,10 @@ resource "aws_subnet" "public2" {
   cidr_block              = "10.0.2.0/24"
   availability_zone       = var.az2
   map_public_ip_on_launch = true
-  tags = { Name = "public-subnet-2" }
+  tags = {
+    Name                     = "public-subnet-2"
+    "kubernetes.io/role/elb" = "1"
+  }
 }
 
 resource "aws_subnet" "private1" {
@@ -97,6 +114,25 @@ resource "aws_route_table_association" "public2" {
   route_table_id = aws_route_table.public.id
 }
 
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+  tags = { Name = "private-rt" }
+}
+
+resource "aws_route_table_association" "private1" {
+  subnet_id      = aws_subnet.private1.id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "private2" {
+  subnet_id      = aws_subnet.private2.id
+  route_table_id = aws_route_table.private.id
+}
+
 # ----------------------
 # S3 BUCKET FOR UI
 # ----------------------
@@ -108,6 +144,44 @@ resource "random_id" "suffix" {
 resource "aws_s3_bucket" "frontend" {
   bucket = "barcode-ui-${random_id.suffix.hex}"
   tags = { Name = "Frontend UI Bucket" }
+}
+
+resource "aws_s3_bucket_public_access_block" "frontend" {
+  bucket = aws_s3_bucket.frontend.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+# resource "aws_s3_bucket_policy" "frontend" {
+#   bucket = aws_s3_bucket.frontend.id
+#
+#   policy = jsonencode({
+#     Version = "2012-10-17",
+#     Statement = [
+#       {
+#         Sid       = "PublicReadGetObject",
+#         Effect    = "Allow",
+#         Principal = "*",
+#         Action    = ["s3:GetObject"],
+#         Resource  = "${aws_s3_bucket.frontend.arn}/*"
+#       }
+#     ]
+#   })
+# }
+
+resource "aws_s3_bucket_website_configuration" "frontend" {
+  bucket = aws_s3_bucket.frontend.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "index.html"
+  }
 }
 
 # ----------------------
@@ -128,27 +202,34 @@ resource "aws_ecr_repository" "backend" {
 }
 
 # ----------------------
-# EKS CLUSTER (MANUAL IAM ROLES REQUIRED)
+# EKS CLUSTER WITH EC2 NODES IN PRIVATE SUBNETS (ACROSS 2 AZs)
 # ----------------------
 
 resource "aws_eks_cluster" "eks_cluster" {
   name     = var.cluster_name
-  role_arn = "arn:aws:iam::290182369109:role/LabRole" # Replace manually
+  role_arn = "arn:aws:iam::290182369109:role/LabRole"
 
   vpc_config {
-    subnet_ids = [aws_subnet.public1.id, aws_subnet.public2.id]
+    subnet_ids             = [aws_subnet.private1.id, aws_subnet.private2.id]
+    endpoint_public_access = true
   }
 }
 
-resource "aws_eks_fargate_profile" "default" {
-  cluster_name           = aws_eks_cluster.eks_cluster.name
-  fargate_profile_name   = "default"
-  pod_execution_role_arn = "arn:aws:iam::290182369109:role/LabRole" # Replace manually
-  subnet_ids             = [aws_subnet.private1.id, aws_subnet.private2.id]
+resource "aws_eks_node_group" "default" {
+  cluster_name    = aws_eks_cluster.eks_cluster.name
+  node_group_name = "default-node-group"
+  node_role_arn   = "arn:aws:iam::290182369109:role/LabRole"
+  subnet_ids      = [aws_subnet.private1.id, aws_subnet.private2.id]
 
-  selector {
-    namespace = "default"
+  scaling_config {
+    desired_size = 2
+    max_size     = 2
+    min_size     = 1
   }
+
+  instance_types = ["t3.medium"]
+  ami_type       = "AL2_x86_64"
+  disk_size      = 20
 }
 
 # ----------------------
@@ -177,4 +258,4 @@ output "public_subnet_ids" {
 
 output "private_subnet_ids" {
   value = [aws_subnet.private1.id, aws_subnet.private2.id]
-}  
+}
