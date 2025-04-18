f44e0fcf9889d302a497975bc810ef2f55e000cf
Abu Kalam Babuji <abukalambabuji99@gmail.com>
2025-04-18 11:56:59 -0300

    Chenged the scrip paaths in Index.html, corrected the aws_s3_bucket_website_configuration

--- TF/main.tf
+++ TF/main.tf
@@ -155,32 +155,32 @@ resource "aws_s3_bucket_public_access_block" "frontend" {
   restrict_public_buckets = false
 }
 
-# resource "aws_s3_bucket_policy" "frontend" {
-#   bucket = aws_s3_bucket.frontend.id
-#
-#   policy = jsonencode({
-#     Version = "2012-10-17",
-#     Statement = [
-#       {
-#         Sid       = "PublicReadGetObject",
-#         Effect    = "Allow",
-#         Principal = "*",
-#         Action    = ["s3:GetObject"],
-#         Resource  = "${aws_s3_bucket.frontend.arn}/*"
-#       }
-#     ]
-#   })
-# }
+resource "aws_s3_bucket_policy" "frontend" {
+  bucket = aws_s3_bucket.frontend.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject",
+        Effect    = "Allow",
+        Principal = "*",
+        Action    = ["s3:GetObject"],
+        Resource  = "${aws_s3_bucket.frontend.arn}/*"
+      }
+    ]
+  })
+}
 
 resource "aws_s3_bucket_website_configuration" "frontend" {
   bucket = aws_s3_bucket.frontend.id
 
   index_document {
-    suffix = "index.html"
+    suffix = "Index.html"
   }
 
   error_document {
-    key = "index.html"
+    key = "Index.html"
   }
 }
 
