4ccc7f56be1e9f277b70a8c3257867f1bf0514e4
Abu Kalam Babuji <abukalambabuji99@gmail.com>
2025-04-18 12:20:18 -0300

    allowed all Orgins in CORS configuration

--- Backend/src/main/java/com/example/nutrition_analyser/Config/WebConfig.java
+++ Backend/src/main/java/com/example/nutrition_analyser/Config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8000") // or "*" during development
+                ..allowedOrigins("*")
                 .allowedMethods("GET", "POST", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
