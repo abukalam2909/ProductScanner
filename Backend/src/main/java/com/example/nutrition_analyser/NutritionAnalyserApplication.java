f0ff721ddcb0d5d06059561cac6bbdfe5907a4f5
Abu Kalam Babuji <abukalambabuji99@gmail.com>
2025-04-20 11:26:16 -0300

    Update .gitignore

--- Backend/src/main/java/com/example/nutrition_analyser/NutritionAnalyserApplication.java
+++ Backend/src/main/java/com/example/nutrition_analyser/NutritionAnalyserApplication.java
@@ -3,7 +3,6 @@ package com.example.nutrition_analyser;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class NutritionAnalyserApplication {
 
